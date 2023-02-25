9122a44ea8501046183e472c3d47642bc3af2451
llyttle <llyttle@olin.edu>
2023-02-25 12:57:21 -0500

    map collisions working, about to convert datastructure from list to dict

--- map-navigation/generate_path.py
+++ map-navigation/generate_path.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+import time
+
 class Node:
   def __init__(self, pos, f=0, g=0, h=0, parent=None):
     self.pos = pos
@@ -14,89 +16,130 @@ class Node:
 
 class path_generator:
   def __init__(self, map):
+    # 2D map for A* operations
     self.map = map
     
+    # 'Color' B&W map to combine with self.overlay
     self.map_img = np.repeat(self.map[:, :, np.newaxis], 3, axis=2)
     
+    # Overlay used for visualizations
     self.overlay = None
+
     self.last_drawn = None
 
     self.fig = plt.figure()
     self.viewer = self.fig.add_subplot(111)
 
-    # self.viewer.tick_params(labelbottom=False)
-    # self.viewer.tick_params(labelleft=False)
+    self.viewer.tick_params(labelbottom=False)
+    self.viewer.tick_params(labelleft=False)
+
 
   def A_star(self, start, end):
     self.overlay = self.create_overlay(start, end)
     self.last_drawn = start
 
-    open = []
-
     open = [Node(start)]
     closed = []
 
     while open:
+      tic = time.perf_counter()
+      
       q = open.pop(open.index(min(open, key=lambda x:x.f)))
-      print("Q: ", q.pos)
-      print("END: ", end)
+      
+      toc1 = time.perf_counter()
 
-      if np.array_equal(q.pos, end) or q.pos[1]>self.map.shape[1]:   # TODO: Add check if pos is within graph
-        print("HOORAY!!!!!")
+      if np.array_equal(q.pos, end):
+        print("Goal Achieved")
         break
       
       # set each neighbor as distance 1 from q
-      descendants = [ Node(q.pos + [-1, 1], g=1+q.g, parent=q),
-                      Node(q.pos + [ 0, 1], g=1+q.g, parent=q),
-                      Node(q.pos + [ 1, 1], g=1+q.g, parent=q),
-                      Node(q.pos + [-1, 0], g=1+q.g, parent=q),
-                      Node(q.pos + [ 1, 0], g=1+q.g, parent=q),
-                      Node(q.pos + [-1,-1], g=1+q.g, parent=q),
-                      Node(q.pos + [ 0,-1], g=1+q.g, parent=q),
-                      Node(q.pos + [ 1,-1], g=1+q.g, parent=q)]
+      descendants = [ Node(q.pos + [-1, 1], parent=q),
+                      Node(q.pos + [ 0, 1], parent=q),
+                      Node(q.pos + [ 1, 1], parent=q),
+                      Node(q.pos + [-1, 0], parent=q),
+                      Node(q.pos + [ 1, 0], parent=q),
+                      Node(q.pos + [-1,-1], parent=q),
+                      Node(q.pos + [ 0,-1], parent=q),
+                      Node(q.pos + [ 1,-1], parent=q)]
 
       # for successor in descendants
       for s in descendants:
-        s.h = self.distance(s.pos, end)
+        # if outside map
+        if s.pos[1]>=self.map.shape[0] or s.pos[0]>=self.map.shape[1]:
+          break
+        # if on an obstacle
+        if self.map[s.pos[1], s.pos[0]] == 0.0:
+          break
 
-        s.f = s.g + s.h
-
+        # if undiscovered
         if s not in open and s not in closed:
-          open.append(s)
+              # s.g = self.distance(s.pos, q.pos) + q.g
+              s.g = 1
+              s.h = self.distance(s.pos, end)
+              s.f = s.g + s.h
+
+              open.append(s)
+        # if discovered previously
         else:
-          if s.g > q.g + 1: #TODO: fix for other distances
-            s.g = q.g + 1
+          print("THIS WILL NEVER PRINT")
+          if s.g > self.distance(s.pos, q.pos)+q.g: #TODO: fix for other distances
+            # s.g = self.distance(s.pos, q.pos)+q.g
+            s.g = 1
+            s.h = self.distance(s.pos, end)
+            s.f = s.g + s.h
 
             if s in closed:
-              closed.remove(closed.index(s))
+              # print("==============================")
+              # print("CLOSED BEFORE POP: ", len(open))
+              # print("CLOSED AFTER POP: ", len(open))
+              closed.pop(closed.index(s))
               open.append(s)
 
-      closed.append(q)
+        self.update_overlay(s.pos, np.array([.7,.7,.7]))
 
-      self.update_overlay(q.pos)
+        # print(len(open))
+        # print(len(closed))
+
+      closed.append(q)
+      toc2 = time.perf_counter()
+        
+      self.update_overlay(q.pos, np.array([0, 1, 0]))
+      self.update_overlay(self.last_drawn, np.array([1,1,0]))
+      self.last_drawn = q.pos
+
+      self.show()
+
+      toc3 = time.perf_counter()
+      
+      # print("OPEN LENGTH", len(open))
+      # print("CLOSED LENGTH", len(closed))
+      # print("TimeDelta1: ", toc1 - tic)
+      # print("TimeDelta2: ", toc2 - toc1)
+      # print("TimeDelta3: ", toc3 - toc2)
     
   def distance(self, p1, p2):
     return np.linalg.norm(p1 - p2)
 
-  def update_overlay(self, point):
-    self.overlay[point[1], point[0], :] = np.array([100, 255, 0])
-    self.overlay[self.last_drawn[1], self.last_drawn[0], :] = np.array([0, 255, 0])
+  def update_overlay(self, point, color):
+    self.overlay[point[1], point[0], :] = color
     
-    self.viewer.imshow(self.map_img*self.overlay, interpolation='nearest')
-
-    self.last_drawn = point
+  def show(self):
+    self.viewer.clear()
+    self.viewer.imshow((self.map_img*self.overlay).astype('uint8'), interpolation='nearest')
 
     self.fig.canvas.draw()
-    plt.pause(.05)
+    plt.pause(.01)
 
   def create_overlay(self, start, end):
-    overlay = np.zeros(np.append(self.map.shape,3))+255   #Add 3 layers to create an RGB image
+    overlay = np.ones(np.append(self.map.shape,3))   #Add 3 layers to create an RGB image
 
-    overlay = cv2.circle(overlay, start, 0, (255, 0, 0), 1)
-    overlay = cv2.circle(overlay, end, 0, (255, 0, 0), 1)
+    overlay = cv2.circle(overlay, start, 0, (1, .5, 0), 1)
+    overlay = cv2.circle(overlay, end, 0, (1, .5, 0), 1)
+
+    print("PLOTTED END: ", end)
 
     # Works at resolution: 100
-    cv2.putText(overlay, "Start", start - np.array([-5,0]), cv2.FONT_HERSHEY_SIMPLEX, .25, (0,0,255), 1)
-    cv2.putText(overlay, "End", end - np.array([-5,0]),     cv2.FONT_HERSHEY_SIMPLEX, .25, (0,0,255), 1)
+    cv2.putText(overlay, "Start", start - np.array([-5,0]), cv2.FONT_HERSHEY_SIMPLEX, .25, (0,0,0.5), 1)
+    cv2.putText(overlay, "End", end - np.array([-5,0]),     cv2.FONT_HERSHEY_SIMPLEX, .25, (0,0,0.5), 1)
 
     return overlay
--- map-navigation/main.py
+++ map-navigation/main.py
@@ -4,10 +4,10 @@ from generate_map import map_generator
 from generate_path import path_generator
 
 if __name__ == "__main__":
-  M = map_generator(np.array([2.1,3.3]), 20)
+  M = map_generator(np.array([2.1,3.3]), 50)
   # M.show(overlay=True)
 
   P = path_generator(M.map)
 
-  P.A_star(np.array([5,5]), np.array([50,20]))
+  P.A_star(np.array([5,5]), np.array([100,70]))
   # P.show(overlay=True)
